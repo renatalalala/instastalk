a83c15f05e3a6642492b509e4a610de32eb457a9
Renata Santillan <renata.santillan@gmail.com>
2013-06-18 14:23:42 -0700

    initial commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,40 @@
+enable :sessions
+get '/' do
+	erb :index
+end
+
+get '/oauth/connect' do
+	redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+end
+
+get '/oauth/callback' do
+	response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+	session[:access_token] = response.access_token
+	redirect '/homepage'
+end
+
+
+get '/homepage' do
+	erb :homepage
+end
+
+post '/homepage' do 
+	user = Instagram.user_search("#{params[:username]}").pop
+	p user.username
+	redirect "/profile/#{user.username}"
+end
+
+get '/instapics' do
+  user = Instagram.client(:access_token => session[:access_token])
+  @user = user.user
+  erb :instapics
+end
+
+
+get '/profile/:username' do 
+	@user = Instagram.user_search(params[:username]).pop
+	erb :profile
+end
+
+
+
